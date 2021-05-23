6d58f2c701f43f13b86daf5add0f50648dc44015
chenwoyao <chenwoyao@163.com>
2021-05-23 11:38:30 +0800

    add

--- upload-and-run.sh
+++ upload-and-run.sh
@@ -1,5 +1,5 @@
 # 第一次的运行
 # scp -r . root@121.5.231.10:/usr/woyao/woyaoAlbum
 # 之后只需要上传uploads下本次新增的图片就好
-scp -r ./uploads/users root@121.5.231.10:/usr/woyao/woyaoAlbum/uploads/
+scp -r ./uploads/bale root@121.5.231.10:/usr/woyao/woyaoAlbum/uploads/
 ssh root@121.5.231.10 'sh /usr/woyao/woyaoAlbum/run.sh'
